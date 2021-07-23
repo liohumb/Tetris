a35aae85a963308f0269bfb8e4c23660b9fdc740
Lionel Humbert <helloliohumb@gmail.com>
2021-07-23 08:37:43 +0200

    mise en place variable

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector ('#score')
+    const startBtn = document.querySelector('#start-button')
+    const width = 10
+    let nextRandom = 0
+    let timerId
+    let score = 0
+    const colors = [
+        'orange',
+        'red',
+        'purple',
+        'green',
+        'blue'
+    ]
+
+    
+
+})
