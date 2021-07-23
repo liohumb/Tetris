6c915ebe4bcf929c91279b8886b51925b0f9a78f
Lionel Humbert <helloliohumb@gmail.com>
2021-07-23 09:16:42 +0200

    fonction moveDown

--- assets/js/app.js
+++ assets/js/app.js
@@ -90,6 +90,11 @@ document.addEventListener('DOMContentLoaded', () => {
 
       document.addEventListener('keyup', control)
 
-      
+      function moveDonw() {
+          undraw()
+          currentPosition += width
+          draw()
+          freeze()
+      }
 
 })
